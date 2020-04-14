79a3b25228916d115e41b0293d909e769c32ce5a
canersu <canersu34@gmail.com>
2020-04-14 13:04:36 +0300

    SLAM with gmapping and amcl

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -2,7 +2,7 @@
 
 xterm  -e  "source ~/.bashrc; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm  -e  "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm  -e  "roslaunch turtlebot_gazebo mapping.launch" &
 sleep 2
 xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 1
