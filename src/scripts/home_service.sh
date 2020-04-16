4f6ad5b022beccebcee997b06a292e47f337193d
canersu <canersu34@gmail.com>
2020-04-17 01:56:52 +0300

    environment update

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-xterm  -e "source ~/.bashrc; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/map/office_map.yaml
+
+xterm  -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 2
--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-xterm  -e "source ~/.bashrc; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/map/office_map.yaml
+
+xterm  -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 2
 xterm  -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 2
 xterm  -e "roslaunch pick_objects pick_objects.launch" &
-
